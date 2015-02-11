55494e642324fb0b2841478e62e9ec5c652f21cd
Bern67 <h2o.bern@gmail.com>
2015-02-10 16:19:07 -0800

    summary

--- pca.R
+++ pca.R
@@ -5,20 +5,21 @@ fs_lgh <- read.csv("fs_fish_length.csv")
 fs_smry <- read.csv("fs_fish_smry.csv")
 fs_hab <- read.csv("fs_habitat.csv")
 
+#CT & DV
 fish<-fs_fish[c(1:3,17:18)]
 rm(fs_fish)
 fish <-na.omit(fish)
-
 library(dplyr)
 ave.rel.den <- fish%>% # combines species for mean relative fish density
   group_by(Site)%>%
   summarise(ave.den=mean(Species_Density..fish.100sq.m.))
 
-
+#Fish length
 length <- fs_lgh[-c(2:3,6:7)]#2009 fish
 rm(fs_lgh)
 
-hab <-fs_hab[c(1,7:9,12:14,16:18,20:28,30:31)]
+#Habitat
+hab <-fs_hab[c(1,7:9,12:14,16:18,20,22, 24,26:28,30:31)]
 hab <-na.omit(hab)
 rm(fs_hab)
 str(hab)
@@ -35,14 +36,23 @@ cor.matrix(scale(hb))
 cor(log(hb+1))
 hb.lg <- log(hb+1)
 
+#Summary
+smry <-fs_smry[c(1,4,6:7,11:15)]
+rm(fs_smry)
+all <-left_join(smry, ave.rel.den) #includes ave density of fish
+rm(smry)
+
+#----------
+
+#PCA 
 require(MASS) #loads the PCA package
 pca <- princomp(scale(hb.lg)) #creates a PC matrix using the correlation matrix
-biplot(pca, expand = 1.05,main = "Biplot", xlab = "Comp.1 (38.5%)", ylab = "Comp.2 (20.9%)")
+biplot(pca, expand = 1.05,main = "Biplot", xlab = "Comp.1 (37.9%)", ylab = "Comp.2 (20.4%)")
 #Scale for sites(PC matrix-pca$scores) on top, scale for variables (vectors-loadings) along bottom
 summary(pca) #proportion of variance is eigenvalues for each PC
 
 plot(pca, main="Scree Plot") #Scree plot
-broken.stick(20) #After comparing, keep comp 1 & 2
+broken.stick(18) #After comparing, keep comp 1 & 2
 
 round(loadings(pca),2) #Check eigenvectors: length of vector is relative variance and how much it contributes to the PC
 #Principal component loading (pg 50).  The further from zero, the greater the contribution.
@@ -62,4 +72,4 @@ round(euc,2)
 euc.1<-dist(pc.matrix.1[,c(1,2)])  #calculate Euclidian distance among sites in PCA space using only first 2 PCs
 round(euc.1,2)
 plot(euc,euc.1,main="Shepard diagram", xlab="Distance in Multidimensional space", ylab="Distance in Reduced space")
-#major distortion, not a straight line (the farther from y=x the more distorted)
+#Farther from y=x the more distortion
