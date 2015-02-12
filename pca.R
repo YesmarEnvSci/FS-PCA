8b3358efb85d26e1c9a49043f992f09a3668bf01
Bern67 <h2o.bern@gmail.com>
2015-02-11 23:06:42 -0800

    Channel type gradient

--- pca.R
+++ pca.R
@@ -1,50 +1,46 @@
 #USFS habitat and fish pop analysis
 
-fs_fish <- read.csv("fs_fish.csv")
-fs_lgh <- read.csv("fs_fish_length.csv")
-fs_smry <- read.csv("fs_fish_smry.csv")
-fs_hab <- read.csv("fs_habitat.csv")
-
 #CT & DV
+fs_fish <- read.csv("fs_fish.csv")
 fish<-fs_fish[c(1:3,9, 17:18)]
 rm(fs_fish)
 fish <-na.omit(fish)
-
 library(dplyr)
 pop <- fish%>% 
   group_by(Site, Fish.Species)%>%
   summarise(pop.ave=mean(Pop_Est))
 
-
 #Fish length
+fs_lgh <- read.csv("fs_fish_length.csv")
 length <- fs_lgh[-c(2:3,6:7)]#2009 fish
-rm(fs_lgh)
-
 fk.l <-length%>%
   group_by(site, species)%>%
   summarise(fk.ave=mean(fk.length..mm.))
+rm(fs_lgh)
 rm(length)
 
-
 #Summary
+fs_smry <- read.csv("fs_fish_smry.csv")
 smry <-fs_smry[c(1,4,6:7,11:15)]
 rm(fs_smry)
-all <-left_join(smry, ave.rel.den) #includes ave density of fish
-rm(smry)
 
 #Habitat
-hab <-fs_hab[c(1,7:9,12:14,16:18,20:21,26:28,30:31)]
+fs_hab <- read.csv("fs_habitat.csv")
+hab <-fs_hab[c(1,6:9,12:14,16:18,20:21,26:28,30:31)]
 hab <-na.omit(hab)
 rm(fs_hab)
-str(hab)
 ave.hab <-hab%>%
   group_by(Site)%>%
   summarise_each(funs(mean))
-rm(hab)  
-hb <-ave.hab
+  
+hab.1 <-ave.hab[-c(1,2)]
+hab.l <-scale(log(hab.1+1))
+boxplot(scale(hab.1))
+boxplot(hab.l)
 
 #Data for analysis that includes relative fish abundance
-hb_fish <-left_join(hb,all,by="Site")
+#-------
+hb_fish <-left_join(hab,fish,by="Site")
 hbfish <-hb_fish[-c(1,3,18:25)]
 rm(hb_fish)
 hbfish <-na.omit(hbfish)
@@ -56,17 +52,15 @@ cor.matrix(scale(hb))
 
 cor(log(hbfish+1))
 
-
 lshap <- lapply(hbfish.lg, shapiro.test) #shapiro test on log transformed data
 lres <- sapply(lshap, `[`, c("statistic","p.value"))
 t(lres)
 
-#----------
-
 #PCA 
+#----------
 require(MASS) #loads the PCA package
-pca <- princomp(scale(hbfish.lg)) #creates a PC matrix using the correlation matrix
-biplot(pca, expand = 1.05,main = "Biplot", xlab = "Comp.1 (37.9%)", ylab = "Comp.2 (20.4%)")
+pca <- princomp(hab.l, cor=TRUE) #creates a PC matrix using the correlation matrix
+biplot(pca, expand = 1.05,main = "Biplot", xlab = "Comp.1 (38.0%)", ylab = "Comp.2 (19.0%)")
 #Scale for sites(PC matrix-pca$scores) on top, scale for variables (vectors-loadings) along bottom
 summary(pca) #proportion of variance is eigenvalues for each PC
 
@@ -81,10 +75,16 @@ round((pca$scores),2) #PC matrix showing site scores for all PCs. How far each i
 #This is the distribution of PC1 and PC2 site scores (top scale).  Each variable for each component. 
 #In this case due to broken stick, PC1 and PC2
 
+#channel type plot
+#-----
+
+plot(pca$scores[,1], pca$scores[,2],xlab="PC 1", ylab="PC 2", type='n') # plot the first two PCs.  Can you interpret the plot?
+text(pca$scores[,1], pca$scores[,2],labels=smry$Chan.Type, lwd=2)
+
+#create Shepard diagram
 #---------
-#create shepard diagram
 head(round(pca$scores,2))
-euc<-dist(hbfish.lg)#Computes the distance matrix in the log transformed data (mutidimensional space)
+euc<-dist(hab.l)#Computes the distance matrix in the log transformed data (multidimensional space)
 euc.1<-dist(pca$scores[,c(1,2)]) #Computes the distance matrix in the PC matrix (reduced space)
-plot(euc,euc.1,main="PC=2", xlab="Distance in Multidimensional space", ylab="Distance in Reduced space")   #Shepard diagram
+plot(euc,euc.1,main="Shepards Diagram (PC=2)", xlab="Distance in Multidimensional space", ylab="Distance in Reduced space")   #Shepard diagram
 
